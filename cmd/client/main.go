20da37bc9d0d9c12f1a9534c9875d69b2925d395
shun <warnshun@gmail.com>
2023-09-19 14:46:59 +0800

    feat: JWT authentication

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"time"
 
 	"github.com/warnshun/pcbook/client"
 	"github.com/warnshun/pcbook/pb"
@@ -74,18 +75,51 @@ func testRateLaptop(laptopClient *client.LaptopClient) {
 
 }
 
+func authMethods() map[string]bool {
+	const laptopServicePath = "/LaptopService/"
+
+	return map[string]bool{
+		laptopServicePath + "CreateLaptop": true,
+		laptopServicePath + "UploadImage":  true,
+		laptopServicePath + "RateLaptop":   true,
+	}
+}
+
+const (
+	username        = "admin1"
+	password        = "myPassword"
+	refreshDuration = 30 * time.Second
+)
+
 func main() {
 	serverAddress := flag.String("address", "", "The server address")
 	flag.Parse()
 	log.Printf("Dial server on %s", *serverAddress)
 
-	conn, err := grpc.Dial(*serverAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	cc1, err := grpc.Dial(*serverAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatal("cannot dial server:", err)
 	}
-	defer conn.Close()
+	defer cc1.Close()
 
-	laptopClient := client.NewLaptopClient(conn)
+	authClient := client.NewAuthClient(cc1, username, password)
+	interceptor, err := client.NewAuthInterceptor(authClient, authMethods(), refreshDuration)
+	if err != nil {
+		log.Fatal("cannot create auth interceptor:", err)
+	}
+
+	cc2, err := grpc.Dial(
+		*serverAddress,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithUnaryInterceptor(interceptor.Unary()),
+		grpc.WithStreamInterceptor(interceptor.Stream()),
+	)
+	if err != nil {
+		log.Fatal("cannot dial server:", err)
+	}
+	defer cc2.Close()
+
+	laptopClient := client.NewLaptopClient(cc2)
 
 	// testCreateLaptop(laptopClient)
 	// testSearchLaptop(laptopClient)
